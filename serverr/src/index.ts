e6baf80008c44a5692f3441b590cd662b6698bd4
Parveen1374 <prvnk1374@gmail.com>
2025-08-03 17:32:08 +0530

    Applied cors middleware to allow cross-origin requests from the frontend URL.

--- serverr/src/index.ts
+++ serverr/src/index.ts
@@ -8,7 +8,7 @@ const PORT = 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://doctor-appointment-dhv5.onrender.com/", // Replace with your frontend URL
+    origin: "https://doctor-appointment-dhv5.onrender.com", // Replace with your frontend URL
     credentials: true,
   })
 );
