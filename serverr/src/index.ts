07a6f73869bfc082700e9c68214d78d8ea1e99b0
Parveen1374 <prvnk1374@gmail.com>
2025-08-03 17:22:08 +0530

    Cors added to server configuration

--- serverr/src/index.ts
+++ serverr/src/index.ts
@@ -6,7 +6,12 @@ const app = express();
 const PORT = 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://doctor-appointment-dhv5.onrender.com/", // Replace with your frontend URL
+    credentials: true,
+  })
+);
 
 app.use("/doctors", doctorRoutes);
 app.use("/appointments", appointmentRoutes);
